457a07d657adc23e29e2272862b7059d8003b3e8
github-MaxCE <77289389+github-MaxCE@users.noreply.github.com>
2021-09-04 20:54:32 +0100

    simplified the camera x rotation clamping to a single line for better optimization

--- CPMPlayer.cs
+++ CPMPlayer.cs
@@ -135,10 +135,7 @@ public class CPMPlayer : MonoBehaviour
         rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0.02f;
 
         // Clamp the X rotation
-        if(rotX < -90)
-            rotX = -90;
-        else if(rotX > 90)
-            rotX = 90;
+        rotX = Mathf.Clamp(rotX, -90f, 90f);
 
         this.transform.rotation = Quaternion.Euler(0, rotY, 0); // Rotates the collider
         playerView.rotation     = Quaternion.Euler(rotX, rotY, 0); // Rotates the camera
